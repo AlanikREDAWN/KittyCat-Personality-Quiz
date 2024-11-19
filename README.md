abbb6e8a0c56fa5b1cd369acf3b8a4190b9f59ac
Alanik <152527270+AlanikREDAWN@users.noreply.github.com>
2024-11-19 16:18:34 +0000

    i might be done

--- README.md
+++ README.md
@@ -18,36 +18,25 @@ You can extend your project further by:
 - Creating a button to update result instead of having it display unprompted.
 - Disabling buttons after a person selects an answer.
 
+This personality quiz is just a fun little quiz that will try to determine whether you are a movie or book lover based on the attributes of your dream pet! I made it as part of GWC's SPP, and I really enjoyed making it. I hope you enjoy it and find it fun!
 
 ### Image Attributions
 *Tell us where you found the image you are using! Make sure it is in the public domain, has a license that allows you to use it, or is one you took on your own. Check out [archive.org](https://archive.org/), [Pixabay](https://pixabay.com/), [Unsplash](https://unsplash.com/), or [Burst](https://burst.shopify.com/) for free, uncopyrighted images.*
-- [Espresso](https://pixabay.com/photos/coffee-espresso-short-cup-mug-731330/)
-- [Matcha latte](https://pixabay.com/photos/matcha-green-tea-latt%C3%A9-heart-2683990/)
-- [Chocolate Croissant](https://pixabay.com/photos/chocolate-croissant-bakery-hunger-4840779/)
-- [Egg Sammy](https://pixabay.com/photos/bacon-egg-and-cheese-2834906/)
-- [Bed](https://pixabay.com/photos/bed-pillows-headboard-bedroom-2167288/)
-- [Window](https://pixabay.com/photos/window-window-seat-cushions-2227301/)
-
----
-
-## File Overview
-
-### ← README.md
-
-That's this file. This is the place to tell people more about what your website does and how you built it. 
-
-### ← index.html
-
-HTML files are for content! Add your text, images, buttons, and more using HTML tags.
-
-### ← style.css
-
-CSS files add styling rules to your content like changing colors and fonts. 
-
-### ← script.js
-
-JavaScript files add interactivity to your website using code. This is where you can listen for events, then take an action.
-
-### ← assets
-
-Drag in assets, like images or music, to add them to your project. You do not need to add anything to this folder unless you are working on the extensions. If you are adding an image, it is best to save it to your computer, then upload the file to the `assets` folder. The url of the image will be `assets/example.jpg`.
+- [Cat]()
+- [German Shepards]()
+- [Group of girls]()
+- [Girl on cliff]()
+- [Blue Sky]()
+- [Green grassy landscape]()
+- [Zebra stripes]()
+- [Leopard spots]()
+- [Sticks on the beach]()
+- [Tennis balls]()
+- [Two white lions snuggling]()
+- [Two dogs playing]()
+- [Dog/Cat food]()
+- [Cat getting pets]()
+- [Cat dressed up]()
+- [Brown dog]()
+- [Orange]()
+- [Gray]()
--- script.js
+++ script.js
@@ -19,6 +19,10 @@ var booksScore = 0;
 
 var questionsAnswered = 0;
 
+var result = document.getElementById("result");
+
+var restart = document.getElementById("restart");
+
 var q1a1 = document.getElementById("q1a1");
 var q1a2 = document.getElementById("q1a2");
 
@@ -28,25 +32,205 @@ var q2a2 = document.getElementById("q2a2");
 var q3a1 = document.getElementById("q3a1");
 var q3a2 = document.getElementById("q3a2");
 
+var q4a1 = document.getElementById("q4a1");
+var q4a2 = document.getElementById("q4a2");
+
+var q5a1 = document.getElementById("q5a1");
+var q5a2 = document.getElementById("q5a2");
+
+var q6a1 = document.getElementById("q6a1");
+var q6a2 = document.getElementById("q6a2");
+
+var q7a1 = document.getElementById("q7a1");
+var q7a2 = document.getElementById("q7a2");
+
+var q8a1 = document.getElementById("q8a1");
+var q8a2 = document.getElementById("q8a2");
+
+var q9a1 = document.getElementById("q9a1");
+var q9a2 = document.getElementById("q9a2");
+
 q1a1.addEventListener("click", books);
 q1a2.addEventListener("click", movies);
 
 q2a1.addEventListener("click", movies);
 q2a2.addEventListener("click", books);
 
-q3a1.addEventListener("click", movies);
-q3a2.addEventListener("click", books);
+q3a1.addEventListener("click", books);
+q3a2.addEventListener("click", movies);
+
+q4a1.addEventListener("click", books);
+q4a2.addEventListener("click", movies);
+
+q5a1.addEventListener("click", movies);
+q5a2.addEventListener("click", books);
+
+q6a1.addEventListener("click", books);
+q6a2.addEventListener("click", movies);
+
+q7a1.addEventListener("click", books);
+q7a2.addEventListener("click", movies);
+
+q8a1.addEventListener("click", movies);
+q8a2.addEventListener("click", books);
+
+q9a1.addEventListener("click", movies);
+q9a2.addEventListener("click", books);
+
+
+q1a1.addEventListener("click", disableQuestion1);
+q1a2.addEventListener("click", disableQuestion1);
+
+q2a1.addEventListener("click", disableQuestion2);
+q2a2.addEventListener("click", disableQuestion2);
+
+q3a1.addEventListener("click", disableQuestion3);
+q3a2.addEventListener("click", disableQuestion3);
+
+q4a1.addEventListener("click", disableQuestion4);
+q4a2.addEventListener("click", disableQuestion4);
+
+q5a1.addEventListener("click", disableQuestion5);
+q5a2.addEventListener("click", disableQuestion5);
+
+q6a1.addEventListener("click", disableQuestion6);
+q6a2.addEventListener("click", disableQuestion6);
+
+q7a1.addEventListener("click", disableQuestion7);
+q7a2.addEventListener("click", disableQuestion7);
+
+q8a1.addEventListener("click", disableQuestion8);
+q8a2.addEventListener("click", disableQuestion8);
+
+q9a1.addEventListener("click", disableQuestion9);
+q9a2.addEventListener("click", disableQuestion9);
+
+restart.addEventListener("click", reset);
 
 function books() {
     booksScore += 1;
     questionsAnswered += 1;
 
     console.log("Questions answered = " + questionsAnswered + " Books score = " + booksScore);
+
+    if (questionsAnswered == 9) {
+        console.log("The quiz is done!");
+        updateResult();
+    }
+    
 }
+
+
 
 function movies() {
     moviesScore += 1;
     questionsAnswered += 1;
 
     console.log("Questions answered = " + questionsAnswered + " Movies score = " + moviesScore);
+
+    if (questionsAnswered == 9) {
+        console.log("The quiz is done!");
+        updateResult();
+    }
+}
+
+function updateResult() {
+    if (moviesScore >= 5) {
+        result.innerHTML = "You are a movie person!";
+        console.log("You are a movie person!");
+    }
+    if (booksScore >= 5) {
+        result.innerHTML = "You are a book person!";
+        console.log("You are a book person!");
+    }
+}
+
+function reset() {
+    result.innerHTML = "Your result is...";
+    moviesScore = 0;
+    booksScore = 0;
+    questionsAnswered = 0;
+    console.log("MoviesScore = " + moviesScore);
+    console.log("BooksScore = " + booksScore);
+    console.log("questionsAnswered = " + questionsAnswered);
+    enableAllButtons();
+}
+
+function disableQuestion(button1, button1) {
+    button1.disabled = true;
+    button2.disabled = true;
+}
+
+function disableQuestion1() {
+    q1a1.disabled = true;
+    q1a2.disabled = true;
+}
+
+function disableQuestion2() {
+    q2a1.disabled = true;
+    q2a2.disabled = true;
+}
+
+function disableQuestion3() {
+    q3a1.disabled = true;
+    q3a2.disabled = true;
+}
+
+function disableQuestion4() {
+    q4a1.disabled = true;
+    q4a2.disabled = true;
+}
+
+function disableQuestion5() {
+    q5a1.disabled = true;
+    q5a2.disabled = true;
+}
+
+function disableQuestion6() {
+    q6a1.disabled = true;
+    q6a2.disabled = true;
+}
+
+function disableQuestion7() {
+    q7a1.disabled = true;
+    q7a2.disabled = true;
+}
+
+function disableQuestion8() {
+    q8a1.disabled = true;
+    q8a2.disabled = true;
+}
+
+function disableQuestion9() {
+    q9a1.disabled = true;
+    q9a2.disabled = true;
+}
+
+function enableAllButtons() {
+    q1a1.disabled = false;
+    q1a2.disabled = false;
+
+    q2a1.disabled = false;
+    q2a2.disabled = false;
+
+    q3a1.disabled = false;
+    q3a2.disabled = false;
+
+    q4a1.disabled = false;
+    q4a2.disabled = false;
+
+    q5a1.disabled = false;
+    q5a2.disabled = false;
+
+    q6a1.disabled = false;
+    q6a2.disabled = false;
+
+    q7a1.disabled = false;
+    q7a2.disabled = false;
+
+    q8a1.disabled = false;
+    q8a2.disabled = false;
+
+    q9a1.disabled = false;
+    q9a2.disabled = false;
 }
